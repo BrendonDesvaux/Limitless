4fce4b109054c7b747d7034825b260c75b3e97e7
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2022-08-18 22:30:31 +0200

    Limitless V0.0.1
    Added: Terrain génération (100x100 atm)
    Player with mouvement set, camera follow and rotation

---
+++ Limitless/Assets/script/mouvements.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvements : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool isGrounded = false;
+    public float jumpAmount = 10;
+    public float movementSpeed = 10;
+    public float runningSpeed = 20;
+    public float turningSpeed = 60;
+    public float jumpHeight = 1.5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //if player collider is collinding with groung
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if collider is ground
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+    void Update()
+    {
+        
+        if(isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * jumpAmount, ForceMode.Impulse);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                movementSpeed = runningSpeed;
+            }
+            else
+            {
+                movementSpeed = 10;
+            }
+        }
+        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+    }
+
+
+}
