e3aa76e71ff9d8dbafc5731459a0195d446c982e
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2022-08-19 03:40:50 +0200

    update zoom on cam

--- Limitless/Assets/script/PlayerTracker.cs
+++ Limitless/Assets/script/PlayerTracker.cs
@@ -8,6 +8,13 @@ public class PlayerTracker : MonoBehaviour
     public GameObject target;
     public float rotateSpeed = 5;
     Vector3 offset;
+    public Transform parentObject;
+    public float zoomLevel;
+    public float sensitivity = 1;
+    public float speed = 30;
+    public float maxZoom = 20;
+    float zoomPosition;
+    float rotation = 0.2f;
 
     void Start()
     {
@@ -25,5 +32,11 @@ public class PlayerTracker : MonoBehaviour
         transform.position = target.transform.position - (rotation * offset);
 
         transform.LookAt(target.transform);
+
+        zoomLevel += Input.mouseScrollDelta.y * sensitivity;
+        zoomLevel = Mathf.Clamp(zoomLevel, 0, maxZoom);
+        zoomPosition = Mathf.MoveTowards(zoomPosition, zoomLevel, speed * Time.deltaTime);
+        transform.position = parentObject.position - (transform.forward * zoomPosition);
+
     }
 }
