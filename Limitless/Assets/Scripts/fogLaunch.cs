d9705a6efee3a1eb5b3c23fed526d0343b850638
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2022-11-01 18:37:29 +0100

    Update 1.2.0
    Added dynamics clouds and sandstorm going on every 30 secs to test (changeable easily)

---
+++ Limitless/Assets/Scripts/fogLaunch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class fogLaunch : MonoBehaviour
+{
+    public float timePassed = 0.0f;
+    private ParticleSystem ps;
+
+    private void Start()
+    {
+
+        ps = GetComponent<ParticleSystem>();
+
+    }
+    void Update()
+    {
+        var main = ps.main;
+        Time.timeScale = 1f;
+        timePassed += Time.deltaTime;
+        
+        if (timePassed < 30f)
+        {
+            main.startSize = 0f;
+        }
+        if (timePassed > 30f)
+        {
+            main.startSize = 200f;
+        }
+        if( timePassed > 60F)
+        {
+            timePassed = 0f;
+        }
+    }
+}
