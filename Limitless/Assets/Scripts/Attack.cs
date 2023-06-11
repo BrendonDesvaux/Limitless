4d8191ecc69e26f307c1c4310b4e089598d52483
Polonit <louis.delavenne@algosup.com>
2023-06-11 18:20:00 +0200

    PATH IN SAND

--- Limitless/Assets/Scripts/Attack.cs
+++ Limitless/Assets/Scripts/Attack.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public float damage;
+    public GameObject sphere;
     // if L is pressed, instantiate a sphere and launch it forward, destroying it after 1 second
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere = Instantiate(sphere);
             sphere.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-            sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-            sphere.AddComponent<Rigidbody>();
+
+
+
             sphere.GetComponent<Rigidbody>().AddForce(transform.forward * 2000);
             // add script Weapon with damages equal to 10
-            sphere.AddComponent<Weapon>().damage = 10;//damage;
+            sphere.GetComponent<Weapon>().damage = 10;//damage;
             sphere.tag = "Weapon";
-            Destroy(sphere, 5);
+            Destroy(sphere, 5f);
         }
     }
 }
